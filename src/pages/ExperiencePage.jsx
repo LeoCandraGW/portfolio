07bc0164630da51136921314a0c9d5005992b717
Leo Candra <leocandra1609@gmail.com>
2024-10-23 13:52:22 +0700

    exxperience v1

--- src/pages/ExperiencePage.jsx
+++ src/pages/ExperiencePage.jsx
@@ -6,12 +6,12 @@ function SkillPage() {
     <section className="experience" id="Experience">
       <div className="skill-page">
         <div className="card-diskominfo">
-          <h1>Dinas Komunikasi Dan Informatika Kota Salatiga</h1>
+          <h1>DISKOMINFO SALATIGA</h1>
           <p>internship web developer</p>
           <p>"8 months, from August 2021 to March 2022."</p>
         </div>
         <div className="card-sritex">
-          <h1>Sri Rejeki Isman</h1>
+          <h1>SRITEX</h1>
           <p>FrontEnd Developer</p>
           <p>"I'm currently working in this position since September 2023."</p>
         </div>
--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,11 +1,45 @@
 import React from "react";
 import "../styles/project.css";
+import { motion } from "framer-motion";
 function ProjectPage() {
   return (
     <section className="project" id="Project">
       <div className="project-page">
-        <div className="card">
-          <h1>Under Maintenance</h1>
+        <div className="card-calculator">
+          <motion.img
+            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/flutter/flutter-original.svg"
+            title="Flutter"
+            alt="Flutter"
+            width="60"
+            height="60"
+          />
+        </div>
+        <div className="card-mobile-note">
+          <motion.img
+            src="https://raw.githubusercontent.com/devicons/devicon/master/icons/flutter/flutter-original.svg"
+            title="Flutter"
+            alt="Flutter"
+            width="60"
+            height="60"
+          />
+        </div>
+        <div className="card-note">
+          <motion.img
+           src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"
+           title="React"
+           alt="React"
+            width="60"
+            height="60"
+          />
+        </div>
+        <div className="card-forum">
+          <motion.img
+           src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"
+           title="React"
+           alt="React"
+            width="60"
+            height="60"
+          />
         </div>
       </div>
     </section>
